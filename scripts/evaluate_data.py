73ccbd8a84aca4fd334e92e7be10baac2b6a6a8e
Dingdong-LIU <dliuak@connect.ust.hk>
2022-08-27 15:54:33 +0800

    add: prediction results fill into web json file

--- scripts/evaluate_data.py
+++ scripts/evaluate_data.py
@@ -6,11 +6,13 @@ def substring(string1, string2, fuzz=False):
         s = string1
         string1 = string2
         string2 = s
+    string1 = string1.strip(',. ').replace('\n', ' ')
+    string2 = string2.strip(',. ').replace('\n', ' ')
     if string1 in string2:
         return True
     if fuzz:
         pr = partial_ratio(string1, string2)
-        if pr > 90:
+        if pr > 85:
             return True
     return False
 
@@ -27,11 +29,29 @@ def locate_post(list_of_posts, target_post_content, start_index=0):
     #         f"'{string1}'\n\nis not a substring of\n\n'{string2}', with partio_ratio {pr}")
     #     print(f"Substring mismatch,\n{verr}")
     except KeyError as kerr:
-        print(f"Key 'content' not found in the post {i}")
+        print(f"Key 'content' not found in the post {i}. With err {kerr}")
     return -1
 
-def create_objects(text_list):
-    return [{'content' : text} for text in text_list]
+def create_objects(text_list, name):
+    assert name in ['claim', 'premise'], f"name should be claim or premise, but your input is {name}"
+    if name == 'claim':
+        output = [
+            {
+            'content' : text,
+            'claimCenter' : '',
+            'claimSentiment' : ''
+            } for text in text_list
+        ]
+    else:
+        output = [
+            {
+                'content' : text,
+                "premiseCenter": "",
+                "supportClaim": "",
+                "supportClaimCenter": ""
+            } for text in text_list
+        ]
+    return output
 
 def insert_objects(posts, insert_objs, index, name):
     for insert_obj in insert_objs:
